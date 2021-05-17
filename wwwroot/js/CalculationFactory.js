39cbf5211474d3a831742f8192a7273cc6b106ac
WangJiaCheng1999 <wanjy171@mymail.unisa.edu.au>
2021-05-18 07:50:44 +0800

    Data summary function

---
+++ wwwroot/js/CalculationFactory.js
@@ -0,0 +1,25 @@
+﻿
+//Average calculation
+//values should be an arrary of number
+function aveCal(values) {
+    let total = 0;
+    for (let i = 0; i < values.length; i++) {
+        total += values[i];
+    }
+    return total / values.length;   
+
+}
+
+//Find the biggest number in values and return which Round it is.
+//values should be an arrary of number
+function findBiggestValue(values) {
+    let biggestRound = "";
+    for (let i = 0; i < values.length; i++) {
+        total += values[i];
+    }
+}
+
+//Median calculation
+function medCal() {
+
+}
