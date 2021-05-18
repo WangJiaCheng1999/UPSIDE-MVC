69d7ce2161aff1f8bbf57bb95d9d47e922feabbc
WangJiaCheng1999 <wanjy171@mymail.unisa.edu.au>
2021-05-18 08:16:51 +0800

    Accuracy rate summary

--- wwwroot/js/CalculationFactory.js
+++ wwwroot/js/CalculationFactory.js
@@ -10,13 +10,40 @@ function aveCal(values) {
 
 }
 
-//Find the biggest number in values and return which Round it is.
-//values should be an arrary of number
-function findBiggestValue(values) {
-    let biggestRound = "";
-    for (let i = 0; i < values.length; i++) {
-        total += values[i];
+//Find the biggest number in values from map and return the key(round).
+function findBiggestValue(map) {
+    let biggestValue = 0;
+
+    for (let m of map.entries()) {
+        if (biggestValue == 0) {
+            biggestValue = m[1];
+            biggestRound = "Round1";
+        } else if (m[1] > biggestValue) {
+            biggestValue = m[1];
+            biggestRound = m[0];
+        }
     }
+
+    return (biggestRound);
+
+}
+
+//Find the smallest number in values from map and return the key(round).
+function findBiggestValue(map) {
+    let smallestValue = 0;
+
+    for (let m of map.entries()) {
+        if (smallestValue == 0) {
+            smallestValue = m[1];
+            smallestRound = "Round1";
+        } else if (m[1] < smallestValue) {
+            smallestValue = m[1];
+            smallestRound = m[0];
+        }
+    }
+
+    return (smallestRound);
+
 }
 
 //Median calculation
