188e8565666feee8439dc172276b0db5f4d7ed8b
WangJiaCheng1999 <yojc1999@gmail.com>
2021-05-21 13:34:59 +0800

    Delete the getLength function, and use a new way to summary data

--- wwwroot/js/CalculationFactory.js
+++ wwwroot/js/CalculationFactory.js
@@ -1,6 +1,6 @@
 ﻿
 //Average calculation
-//values should be an arrary of number
+//values should be an array
 function aveCal(values) {
     let total = 0;
     for (let i = 0; i < values.length; i++) {
@@ -10,64 +10,35 @@ function aveCal(values) {
 
 }
 
-//Find the biggest number in values from map and return the key(round).
-function findBiggestValue(map) {
-    let biggestValue = 0;
-
-    for (let m of map.entries()) {
-        if (biggestValue == 0) {
-            biggestValue = m[1];
-            biggestRound = "Round1";
-        } else if (m[1] > biggestValue) {
-            biggestValue = m[1];
-            biggestRound = m[0];
-        }
-    }
-
-    return (biggestRound);
-
-}
-
-//Find the smallest number in values from map and return the key(round).
-function findBiggestValue(map) {
-    let smallestValue = 0;
-
-    for (let m of map.entries()) {
-        if (smallestValue == 0) {
-            smallestValue = m[1];
-            smallestRound = "Round1";
-        } else if (m[1] < smallestValue) {
-            smallestValue = m[1];
-            smallestRound = m[0];
-        }
-    }
-
-    return (smallestRound);
-
-}
-
 //Median calculation
 function medCal(map) {
 
 }
 
-//Get JSON Data from WWWroot and return the data in a Map
+//Get JSON Data from root and return the data in a Map
 function getAllData(TotalRound) {
-    var dataList = new Map();
-    var singleRoundInfor = new Map();
+    let dataList = new Map();
+    
     //Iterate through all the JSON data and save the experimental data to the map
-    for (var i = 1; i < TotalRound + 1; i++) {
+    for (let i = 1; i < TotalRound + 1; i++) {
         $.ajax({
             type: "GET",
             url: "https://localhost:5001/JData/Round" + i + ".json",
             cache: false,
             async: false,
             success: function (data) {
-                var Round = "Round" + i;
-                for (var j = 0; j < data.length; j++) {
-                    timeUseList.push(data[j].TimeUse);
+                let Round = "Round" + i;
+                let roundsInfo = [];
+                for (let j = 0; j < data.length; j++) {
+                    let singleRoundInfo = new Map();
+                    singleRoundInfo.set("TimeUse", data[j].TimeUse);
+                    singleRoundInfo.set("Success", data[j].Success);
+                    singleRoundInfo.set("ImageType", data[j].ImageType);
+                    singleRoundInfo.set("ImageDetail", data[j].ImageDetail);
+                    singleRoundInfo.set("OverTime", data[j].OverTime);
+                    roundsInfo.push(singleRoundInfo);
                 }
-                dataList.set(Round, data);
+                dataList.set(Round, roundsInfo);
             },
             error: function () {
                 console.log("JSON data may not found.")
@@ -76,15 +47,4 @@ function getAllData(TotalRound) {
     }
 
     return dataList;
-}
-
-//Get the length of a Map
-function getMapLength(map) {
-    var length = 0;
-
-    for (let m of map.entries()) {
-        length++;
-    }
-
-    return length;
 }
