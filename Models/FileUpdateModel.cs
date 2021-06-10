a663b7e9f382e5476dec4b61c2e09249152007d8
Xin Lu <luyxy026@mymail.unisa.edu.au>
2021-06-10 08:38:40 +0800

    add new update dada page

---
+++ Models/FileUpdateModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcTest.Models
+{
+ 
+    public static class IFormFileExtensions
+    {
+        public static string GetFilename(this IFormFile file)
+        {
+            return ContentDispositionHeaderValue.Parse(
+                file.ContentDisposition).FileName.ToString().Trim('"');
+        }
+
+        public static async Task<MemoryStream> GetFileStream(this IFormFile file)
+        {
+            MemoryStream filestream = new MemoryStream();
+            await file.CopyToAsync(filestream);
+            return filestream;
+        }
+
+        public static async Task<byte[]> GetFileArray(this IFormFile file)
+        {
+            MemoryStream filestream = new MemoryStream();
+            await file.CopyToAsync(filestream);
+            return filestream.ToArray();
+        }
+    }
+    
+    public class FileInputModel
+    {
+        public IFormFile FileToUpload { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -7,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 
 namespace MvcTest
@@ -25,7 +27,12 @@ namespace MvcTest
         {
             services.AddControllersWithViews();
             //services.AddDirectoryBrowser();
-            
+            services.AddSingleton<IFileProvider>(
+               new PhysicalFileProvider(
+                   Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/JData")));
+
+            services.AddMvc();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
