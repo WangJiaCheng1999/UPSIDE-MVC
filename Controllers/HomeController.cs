ba85d5f5286e78bfa29468cae2ca161c2949184f
Xin Lu <luyxy026@mymail.unisa.edu.au>
2021-06-10 15:17:14 +0800

    change the raw table style

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
 using MvcTest.Models;
 
@@ -38,17 +41,107 @@ namespace MvcTest.Controllers
         {
             return View();
         }
-        public IActionResult UpdateExperiment()
+        private readonly IFileProvider fileProvider;
+       
+        public IActionResult UpdateExperiment() //update the UpdateExperiment interface and add display data
         {
+            var model = new FilesViewModel();
+            foreach (var item in this.fileProvider.GetDirectoryContents("wwwroot/JData"))
+            {
+                model.Files.Add(
+                    new FileDetails { Name = item.Name, Path = item.PhysicalPath });
+            }
             return View();
         }
-        //Open Unity application
-        public IActionResult OpenUnity()
+        public HomeController(IFileProvider fileProvider)
+        {
+            this.fileProvider = fileProvider;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadFiles(List<IFormFile> files)
+        {
+            if (files == null || files.Count == 0)
+                return Content("files not selected");
+
+            foreach (var file in files)
+            {
+                var path = Path.Combine(
+                        Directory.GetCurrentDirectory(), "wwwroot/JData",
+                        file.GetFilename());
+
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+            }
+
+            return RedirectToAction("UpdateExperiment");
+        }
+        public IActionResult Files()
+        {
+
+            var model = new FilesViewModel();
+            foreach (var item in this.fileProvider.GetDirectoryContents("wwwroot/JData"))
+            {
+                model.Files.Add(
+                    new FileDetails { Name = item.Name, Path = item.PhysicalPath });
+            }
+            return View(model);
+
+        }
+
+        public async Task<IActionResult> Download(string filename)
+        {
+            if (filename == null)
+                return Content("filename not present");
+
+            var path = Path.Combine(
+                           Directory.GetCurrentDirectory(),
+                           "wwwroot/JData", filename);
+
+            var memory = new MemoryStream();
+            using (var stream = new FileStream(path, FileMode.Open))
+            {
+                await stream.CopyToAsync(memory);
+            }
+            memory.Position = 0;
+            return File(memory, GetContentType(path), Path.GetFileName(path));
+        }
+
+        private string GetContentType(string path)
+        {
+            var types = GetMimeTypes();
+            var ext = Path.GetExtension(path).ToLowerInvariant();
+            return types[ext];
+        }
+
+        private Dictionary<string, string> GetMimeTypes()
+        {
+            return new Dictionary<string, string>
+            {
+                {".txt", "text/plain"},
+                {".pdf", "application/pdf"},
+                {".doc", "application/vnd.ms-word"},
+                {".docx", "application/vnd.ms-word"},
+                {".xls", "application/vnd.ms-excel"},
+                {".xlsx", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"},
+                {".png", "image/png"},
+                {".jpg", "image/jpeg"},
+                {".jpeg", "image/jpeg"},
+                {".gif", "image/gif"},
+                {".csv", "text/csv"}
+
+            };
+        }
+    
+    //Open Unity application
+    public IActionResult OpenUnity()
         { 
             //Process.Start("C:/Users/Administrator/Desktop/UnityExperiment/UnityExperiment.exe");
            Process.Start("wwwroot/UnityExperiment/UnityExperiment.exe");
             
-            return RedirectToAction("Index");
+            return RedirectToAction("IntroView");
         }
 
         
